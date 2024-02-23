09df128b0b86e9585ae9f2787ed5f8c4b56bee87
Anamika Tiwary <91148156+anamika-20@users.noreply.github.com>
2024-02-23 17:40:37 +0530

    Updated route.ts

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,4 +1,4 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth } from '@auth0/nextjs-auth0';
-
+// Define route handler for authentication endpoint using handleAuth from Auth0 Next.js SDK
 export const GET = handleAuth();
