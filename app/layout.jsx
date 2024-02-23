b48d35068babde5a7bd873da3f0107edfdedbdc9
Anamika Tiwary <91148156+anamika-20@users.noreply.github.com>
2024-02-23 17:39:30 +0530

    Updated layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,17 @@
-
-import { Inter } from "next/font/google";
-
-
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
+// RootLayout component serves as the top-level layout component for the application
 export default function RootLayout({ children }) {
   return (
+    // HTML document structure with lang attribute set to "en"
     <html lang="en">
+      {/* UserProvider component from Auth0 Next.js SDK wraps the application content to provide user authentication context */}
       <UserProvider>
-        <body className=" h-screen w-full flex items-center justify-center">{children}</body>
+        {/* Body of the HTML document with flexbox layout centered both vertically and horizontally */}
+        <body className="h-screen w-full flex items-center justify-center">
+          {/* Render the child components passed to RootLayout */}
+          {children}
+        </body>
       </UserProvider>
     </html>
   );
